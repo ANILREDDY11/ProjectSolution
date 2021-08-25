8e33b5ef7e8b77b50b5df9b574ce4179359480c4
User <User@DESKTOP-P8RS130>
2021-08-25 09:58:10 +0530

    Add project files.

---
+++ ProjectSolution/Controllers/DynamicBlogController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectSolution.Models;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Resources.Media;
+
+namespace ProjectSolution.Controllers
+{
+    public class DynamicBlogCardController : Controller
+    {
+
+        // GET: DynamicBlogCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getBlogCard(FormCollection form)
+        {
+            string query = form["searchInput"];
+            List<BlogCard> BC = new List<BlogCard>();
+            var item = Sitecore.Context.Database.GetItem("{3A399232-1218-4AA2-B544-98AEB9821223}");
+            Sitecore.Data.Fields.MultilistField BlogList = item.Fields["BlogList"];
+            if (BlogList != null)
+            {
+                foreach (Sitecore.Data.Items.Item I in BlogList.GetItems())
+                {
+                    var ID = I.ID.ToString();
+                    var CreatedDate = DateTime.ParseExact(I.Fields["posteddate"].Value, "yyyyMMdd'T'HHmmss'Z'", CultureInfo.InvariantCulture).ToString("dd/MM/yyyy");
+                    var Title = I.Fields["title"].Value;
+                    var SmallDescription = I.Fields["ShortDescription"].Value;
+                    var Image = I.Fields["ArticleSmallImage"].Value;
+
+                    Sitecore.Data.Fields.ImageField imageField = I.Fields["ArticleSmallImage"];
+                    Sitecore.Data.Items.MediaItem image = new Sitecore.Data.Items.MediaItem(imageField.MediaItem);
+                    string imageSrc = Sitecore.StringUtil.EnsurePrefix('/',
+                    Sitecore.Resources.Media.MediaManager.GetMediaUrl(image));
+                    //var Buttontext = item.Fields["CardButtonText"].Value;
+
+                    //string imgTag = String.Format(@"<img src=""{0}"" alt=""{1}"" />", src, image.Alt);
+
+                    BC.Add(new BlogCard(ID, Title, CreatedDate, SmallDescription, imageSrc,image.Alt));
+                }
+            }
+
+           
+
+            if (query != null)
+            {
+                List<BlogCard> results = null;
+
+                results = BC.FindAll(Findtitle);
+
+                bool Findtitle(BlogCard AMp)
+                {
+                    if (AMp.BlogTitle.ToLower().Contains(query.ToLower()) || AMp.ShortDesc.ToLower().Contains(query.ToLower()))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return View("~/Views/ProjectDetails/dynamics.cshtml", results);
+            }
+            return View("~/Views/ProjectDetails/dynamics.cshtml", BC);
+
+        }
+
+    }
+}
---
+++ ProjectSolution/Controllers/SearchController.cs
@@ -0,0 +1,92 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using Sitecore;
+//using Sitecore.Data.Items;
+//using Sitecore.Resources.Media;
+//using ProjectSolution.Models;
+
+
+
+//namespace ProjectSolution.Controllers
+//{
+//    public class SearchController : Controller
+//    {
+//        // GET: Search
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public ActionResult searchPredicate(FormCollection form)
+//        {
+//            string query = form["searchInput"];
+//            List<Sitecore.Data.Items.Item> blogItems = new List<Sitecore.Data.Items.Item>();
+//            List<BlogCard> BlogCardsCollection = new List<BlogCard>();
+//            var rootitem = Sitecore.Context.Database.GetItem("{{E01FF4BB-1481-4D8D-9AC8-7E221C27E665}}}");
+//            var Websitesettings = Sitecore.Context.Database.GetItem("{3A399232-1218-4AA2-B544-98AEB9821223}");
+//            blogItems = rootitem.Axes.GetDescendants().ToList();
+
+           
+
+//            for (int i = 0; i < blogItems.Count; i++)
+//            {
+//                BlogCard BlogModel = new BlogCard();
+//                var imageUrl = string.Empty;
+
+//                Sitecore.Data.Fields.ImageField imageField = blogItems[i].Fields["ArticleSmallImage"];
+//                if (imageField?.MediaItem != null)
+//                {
+//                    var image = new MediaItem(imageField.MediaItem);
+//                    imageUrl = StringUtil.EnsurePrefix('/', MediaManager.GetMediaUrl(image));
+//                    BlogModel.BlogSImage = imageUrl;
+//                }
+//                BlogModel.Category = blogItems[i].Fields["Category"].Value;
+//                BlogModel.BlogTitle = blogItems[i].Fields["Title"].Value;
+
+//                Sitecore.Data.Fields.DateField dateTimeField = blogItems[i].Fields["PostedDate"];
+
+//                string dateTimeString = dateTimeField.Value;
+
+//                DateTime dateTimeStruct = Sitecore.DateUtil.IsoDateToDateTime(dateTimeString);
+//                BlogModel.date = dateTimeStruct.ToShortDateString();
+
+//                BlogModel.ShortDesc = blogItems[i].Fields["ShortDescription"].Value;
+//                BlogModel.LongDesc = blogItems[i].Fields["LongDescription"].Value;
+//                BlogModel.Readonbtn = Websitesettings.Fields["BlogCardButtonText"].Value;
+//                BlogModel.sitecoreItem = blogItems[i];
+//                BlogModel.BlogURL = Sitecore.Links.LinkManager.GetItemUrl(blogItems[i]);
+
+
+//                BlogCardsCollection.Add(BlogModel);
+//            }
+
+//            if (query is null)
+//                query = "hi";
+
+//            List<BlogCard> results = BlogCardsCollection.FindAll(Findtitle);
+
+
+
+//            bool Findtitle(BlogCard bk)
+//            {
+
+//                if (bk.BlogTitle.Contains(query) || bk.Category.Contains(query) || bk.ShortDesc.Contains(query) || bk.LongDesc.Contains(query))
+//                {
+//                    return true;
+//                }
+//                else
+//                {
+//                    return false;
+//                }
+//            }
+
+//            ViewBag.searchCards = results;
+//            return View("~/Views/ProjectDetails/Search.cshtml", ViewBag.searchCards);
+//        }
+//    }
+//}
+    
